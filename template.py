07f584aa9a320786cb3c4364d6d0b69a0af50e18
md-hasan-1 <hasanraza768001@gmail.com>
2025-01-17 18:35:22 +0530

    ..

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ from pathlib import Path
 path_list = [
     ".github/workflows/main.yaml",
     "config/config.yaml",
-    "notebook/stage_01_data_ingestion.ipynb",
+    "notebook/trail.ipynb",
     "src/__init__.py",
     "src/components/__init__.py",
     "src/pipeline/__init__.py",
@@ -15,10 +15,12 @@ path_list = [
     "src/utils/__init__.py",
     "templates/index.html",
     "dvc.yaml",
+    "bentoml.yaml",
     "requirements.txt",
     "setup.py",
     "app.py",
     "main.py",
+    "service.py",
     "README.md",
     ".gitignore",
     "pyproject.toml"
@@ -51,7 +53,9 @@ with open(".gitignore", "a") as file_obj:
 
 # content for requirements.txt
 with open("requirements.txt", "w") as file_obj:
-    file_obj.write("mlflow\ndvc\nbentoml\nipykernel")
+    file_obj.write("mlflow\n", "dvc\n", "bentoml\n", "ipykernel\n", 
+    "pandas\n", "numpy\n", "matplotlib\n", "seaborn\n", "scikitlearn\n", 
+    "xgboost\n", "lightgbm\n", "catboost\n", "box\n", "\n-e .")
 
 try:
     file_list = ["logger", "exception", "setup"]
@@ -76,3 +80,5 @@ except:
 print("""
 -----------------X-----------------
 """)
+
+
